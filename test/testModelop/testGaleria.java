a9b1d6cb116ef6d4b12e5290adf8618fd7b89806
Marlon Stiff <m.londonot@uniandes.edu.co>
2024-05-10 16:05:09 -0500

    empezar test

--- test/testModelop/testGaleria.java
+++ test/testModelop/testGaleria.java
@@ -2,13 +2,49 @@ package testModelop;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 import org.junit.jupiter.api.Test;
 
-class testGaleria {
+import GaleriaEmpleados.Administrador;
+import GaleriaEmpleados.Cajero;
+import GaleriaEmpleados.Operador;
+import GaleriaGestionSesion.Comprador;
+import GaleriaGestionSesion.Usuario;
+import GaleriaModelo.Galeria;
+import GaleriaModelo.Inventario;
 
+import org.junit.jupiter.api.BeforeEach;
+
+class testGaleria {
+	
+	private Galeria galeria;
+	
+	
+	@BeforeEach
+	void before() {
+		Inventario inventario= new Inventario();
+		//se inicia la galeria
+		Galeria galeria= new Galeria(inventario);
+		
+		//se le introducen los 3 empleados de cada dominacion con su usuario
+		
+		Usuario administrador= new Administrador("Administrador", "123");
+		
+		Usuario operador= new Operador("Operador", "123");
+		
+		Usuario cajero= new Cajero("Cajero", "123");
+		
+		galeria.aniadirUsuarioEmpleado(cajero);
+		galeria.aniadirUsuarioEmpleado(administrador);
+		galeria.aniadirUsuarioEmpleado(operador);
+		
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testParticiparSubasta() {
+		Usuario usuario=new Comprador("Mesi", "0126");
+		
+		galeria.participarSubasta(galeria, usuario);
 	}
 
 }
